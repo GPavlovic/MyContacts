182095fbdce4c005495bb62226887d4b0c1cd95e
GPavlovic <gp.goranpavlovic@gmail.com>
2015-06-24 22:16:23 -0400

    Completed S13L11.

---
+++ app/src/main/java/goranpavlovic/mycontacts/Contact.java
@@ -0,0 +1,19 @@
+package goranpavlovic.mycontacts;
+
+/**
+ * Created by Gogo on 6/24/2015.
+ */
+public class Contact
+{
+    private String mName;
+
+    public String getName()
+    {
+        return mName;
+    }
+
+    public void setName(String name)
+    {
+        mName = name;
+    }
+}
--- app/src/main/java/goranpavlovic/mycontacts/ContactListActivity.java
+++ app/src/main/java/goranpavlovic/mycontacts/ContactListActivity.java
@@ -4,6 +4,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class ContactListActivity extends ActionBarActivity
@@ -14,6 +22,68 @@ public class ContactListActivity extends ActionBarActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        // Add temp test contact
+        Contact temp;
+
+        for (int i = 0; i < 30; i++)
+        {
+            temp = new Contact();
+            temp.setName("John Doe" + i);
+            contacts.add(temp);
+        }
+
+        ListView ContactList = (ListView) findViewById(R.id.contact_list_view);
+        ContactList.setAdapter(new ContactAdapter(contacts));
+
+        ContactList.setOnScrollListener(new AbsListView.OnScrollListener()
+        {
+            int previousFirstItem = 0;
+            boolean isHidden = false;
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState)
+            {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
+            {
+                if (firstVisibleItem > previousFirstItem)
+                {
+                    getSupportActionBar().hide();
+                }
+                else if (firstVisibleItem < previousFirstItem)
+                {
+                    getSupportActionBar().show();
+                }
+
+                previousFirstItem = firstVisibleItem;
+            }
+        });
+    }
+
+    private class ContactAdapter extends ArrayAdapter<Contact>
+    {
+        ContactAdapter(ArrayList<Contact> contacts)
+        {
+            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            convertView = super.getView(position, convertView, parent);
+
+            Contact contact = getItem(position);
+
+            TextView nameTV = (TextView) convertView.findViewById(R.id.contact_row_name);
+            nameTV.setText(contact.getName());
+            return convertView;
+        }
     }
 
     @Override
